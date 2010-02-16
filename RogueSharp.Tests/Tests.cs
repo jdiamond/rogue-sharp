2a94e3f3d72c4e192649d0a41624d6720c123317
Jason Diamond <jason@diamond.name>
2010-02-15 20:53:36 -0800

    Added test project and xUnit binaries.

---
+++ RogueSharp.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RogueSharp.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
